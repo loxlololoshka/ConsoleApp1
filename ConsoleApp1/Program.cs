fc040559559d2673bc311dd53e47a2d0d6e1387b
Твердов Артем Александрович <@corp.itstep>
2020-01-10 17:36:21 +0600

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("IT STEP");
+
+            Console.WriteLine("искандербони");
+
+            string name = "ЧИКИБОМБОНИ";
+
+            Console.WriteLine($"menya zovut {name}");
+            Console.Write("Введите хобби");
+            string hobby = Console.ReadLine();
+
+            dfint age  = Convert.ToInt32( Console.ReadLine()  );
+
+            Console.ReadKey();
+        }
+    }
+
+}
